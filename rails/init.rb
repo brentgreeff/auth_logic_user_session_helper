f31642ca1f4084580d4636887e02746cae97d958
Brent Greeff <brentgreeff@Unknown-00-23-6c-95-2b-93.home>
2009-07-16 23:49:50 +0100

    Forgot to require test_help

--- rails/init.rb
+++ rails/init.rb
@@ -2,6 +2,7 @@ require 'auth_logic_user_session_helper'
 ActionController::Base.send(:include, AuthLogic::UserSessionHelper)
 
 if RAILS_ENV.eql? 'test'
+  require 'test_help'
   require 'auth_logic_user_session_helper/auth_logic_test_helper'
   ActiveSupport::TestCase.send(:include, AuthLogic::TestHelper)
 end
