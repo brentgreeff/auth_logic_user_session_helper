0f5630db7cf12aab163504822c00a815ad728c65
Brent Greeff <brentgreeff@brent-greeffs-macbook.local>
2010-10-29 09:04:54 +0100

    Moved the class loading into the lib folder so it will work as a gem.

---
+++ lib/auth_logic_user_session_helper/instance_methods.rb
@@ -0,0 +1,50 @@
+module AuthLogic
+  module UserSessionHelper
+    
+    module InstanceMethods
+      def current_user_session
+        return @current_user_session if defined?(@current_user_session)
+        @current_user_session = UserSession.find
+      end
+      
+      def current_user
+        return @current_user if defined?(@current_user)
+        @current_user = current_user_session && current_user_session.record
+      end
+      
+      def user?
+        return current_user.is_a? User
+      end
+      
+      def no_user?
+        return current_user.nil?
+      end
+      
+      def user_required
+        return if user?
+        
+        store_location
+        flash[:error] = 'Please log in first'
+        redirect_to login_url
+      end
+      
+      def no_user_required
+        return if no_user?
+        
+        flash[:error] = 'You are already logged in'
+        redirect_to root_url
+      end
+      
+      # Redirect back
+      
+      def store_location
+        session[:return_to] = request.request_uri
+      end
+      
+      def redirect_back_or_default(default)
+        redirect_to(session[:return_to] || default)
+        session[:return_to] = nil
+      end
+    end
+  end
+end
